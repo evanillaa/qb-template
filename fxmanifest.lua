ef0e8576aeecca4536e7d515a45930ef0455ee25
vanillaa <80897991+evanillaa@users.noreply.github.com>
2022-01-05 12:10:34 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-Template'
+version '1.0.0'
+
+shared_script 'config.lua'
+server_script 'server/main.lua'
+client_script 'client/main.lua'
+
+lua54 'yes'
