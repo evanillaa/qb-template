d7c7141242bc1f79d574b7effccee269c7d3f41b
vanillaa <80897991+evanillaa@users.noreply.github.com>
2022-01-11 14:04:53 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,24 +4,39 @@ game 'gta5'
 description 'QB-Template https://github.com/evanillaa'
 version '1.0.0'
 
+-- First Template
+
+shared_scripts {
+    'config.lua',
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua' -- Change this to your preferred language
+}
+
+server_script 'server/main.lua'
+client_script 'client/main.lua'
+
+-- Second Template
+
 shared_script 'config.lua'
 server_script 'server/main.lua'
 client_script 'client/main.lua'
 
--- server_scripts {
-    -- '@oxmysql/lib/MySQL.lua',
-    -- 'server/*.lua'
--- }
+-- Third Template
 
---client_script 'client/*.lua'
+shared_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/*.lua'
+}
 
--- ui_page 'html/index.html'
+client_script 'client/*.lua'
 
--- files {
-	-- 'html/*.html',
-	-- 'html/*.js',
-	-- 'html/*.png',
-	-- 'html/*.css',
--- }
+ui_page 'html/index.html'
+
+files {
+	'html/*.html',
+	'html/*.js',
+	'html/*.png',
+	'html/*.css',
+}
 
 lua54 'yes'
