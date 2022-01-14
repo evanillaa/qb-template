17915ed4268bff6cb2312c0183ae7d43c94f7b06
vanillaa <80897991+evanillaa@users.noreply.github.com>
2022-01-14 15:57:12 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,31 +5,9 @@ author 'evanillaa'
 description 'QB-Template https://github.com/evanillaa'
 version '1.0.0'
 
--- First Template
-
-shared_scripts {
-    'config.lua',
-    '@qb-core/shared/locale.lua',
-    'locales/en.lua' -- Change this to your preferred language
-}
-
-server_script 'server/main.lua'
-client_script 'client/main.lua'
-
--- Second Template
-
 shared_script 'config.lua'
 server_script 'server/main.lua'
 client_script 'client/main.lua'
-
--- Third Template
-
-shared_scripts {
-    '@oxmysql/lib/MySQL.lua',
-    'server/*.lua'
-}
-
-client_script 'client/*.lua'
 
 ui_page 'html/index.html'
 
