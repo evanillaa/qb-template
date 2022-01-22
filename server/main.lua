5d1ec53c40e7f5c0fc158eece3cbeba919ee7b41
vanillaa <80897991+evanillaa@users.noreply.github.com>
2022-01-22 20:37:56 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,1 +1,7 @@
 local QBCore = exports['qb-core']:GetCoreObject()
+
+QBCore.Functions.CreateCallback('qb-template:server:GetPermissions', function(source, cb)
+    local src = source
+    local group = QBCore.Functions.GetPermission(src)
+    cb(group)
+end)
